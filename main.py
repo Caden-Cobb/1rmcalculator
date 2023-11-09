2a8f82c49824b375b32259fb91f5f5810fab721a
Caden <90775304+Caden-Cobb@users.noreply.github.com>
2023-11-09 02:42:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import streamlit as st
 
 st.title("1RM Calculator")
 
-st.write("Enter your Weight:")
-weight = st.number_input("", value=0, step=1)
-
-st.write("Enter your Reps:")
-reps = st.number_input("", value=0, step=1)
+weight = st.number_input("Enter your Weight", min_value=0, step=1, value=0)
+reps = st.number_input("Enter your Reps", min_value=0, step=1, value=0)
 
 if st.button("Calculate 1RM"):
     results = weight / (1.0278 - 0.0278 * reps)
     st.write(f"Your 1RM is: {results:.2f}")
+
 
 # 1RM = weight / ( 1.0278 – 0.0278 × reps )
