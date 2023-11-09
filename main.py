dafc5b80924641070ea990e1de62d56a0c5358f8
cobbb.caden <flvs.9th@gmail.com>
2023-11-09 03:47:33 -0500

    update

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ if st.button("Calculate 1RM"):
     results = weight / (1.0278 - 0.0278 * reps)
     st.write(f"Your 1RM is: {results:.2f}")
 
+st.title("Kg to Lbs")
+Kg = st.number_input("Enter Kg Weight", min_value=0, step=1, value=0)
+Lbs = st.number_input("Enter Lbs Reps", min_value=0, step=1, value=0)
 
+if st.button("Convert"):
+    results = Kg * 2.2
+    st.write(f"lbs: {results:.2f}")
